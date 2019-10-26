ca141392c2a801c4f57f2bfe0e04bc72754a326f
crn1 <gluvajic@aol.com>
2019-10-26 12:33:26 +0200

    Fixed syslink for /root

--- INSTALL.sh
+++ INSTALL.sh
@@ -11,11 +11,11 @@ ln -sf "$PWD/xinitrc" ~/.xinitrc
 ln -sf "$PWD/Xresources" ~/.Xresources
 ln -sf "$PWD/profile" ~/.profile
 
-sudo ln -sf "$PWD/vimrc" ~/.vimrc
-sudo ln -sf "$PWD/gitconfig" ~/.gitconfig
-sudo ln -sf "$PWD/xinitrc" ~/.xinitrc
-sudo ln -sf "$PWD/Xresources" ~/.Xresources
-sudo ln -sf "$PWD/profile" ~/.profile
+sudo ln -sf "$PWD/vimrc" /root/.vimrc
+sudo ln -sf "$PWD/gitconfig" /root/.gitconfig
+sudo ln -sf "$PWD/xinitrc" /root/.xinitrc
+sudo ln -sf "$PWD/Xresources" /root/.Xresources
+sudo ln -sf "$PWD/profile" /root/.profile
 
 #Make folders if they don't exist
 mkdir -p ~/.config
@@ -23,17 +23,17 @@ mkdir -p ~/.config/i3
 mkdir -p ~/.config/i3status
 mkdir -p ~/.config/fish
 
-sudo mkdir -p ~/.config
-sudo mkdir -p ~/.config/i3
-sudo mkdir -p ~/.config/i3status
-sudo mkdir -p ~/.config/fish
+sudo mkdir -p /root/.config
+sudo mkdir -p /root/.config/i3
+sudo mkdir -p /root/.config/i3status
+sudo mkdir -p /root/.config/fish
 
 #i3 syslink
 ln -sf "$PWD/config/i3/config" ~/.config/i3/config
 ln -sf "$PWD/config/i3status/config" ~/.config/i3status/config
 
-sudo ln -sf "$PWD/config/i3/config" ~/.config/i3/config
-sudo ln -sf "$PWD/config/i3status/config" ~/.config/i3status/config
+sudo ln -sf "$PWD/config/i3/config" /root/.config/i3/config
+sudo ln -sf "$PWD/config/i3status/config" /root/.config/i3status/config
 
 #Delete motd message
 sudo rm -f /etc/motd
