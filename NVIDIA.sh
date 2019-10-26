d3fe870b835d16ccd2e435940714247844f736c1
crn1 <gluvajic@aol.com>
2019-10-26 20:55:29 +0200

    Modifed echoing

--- NVIDIA.sh
+++ NVIDIA.sh
@@ -1,5 +1,7 @@
 sudo kld_load linux64
-echo "If not working restart computer"
+echo ""
+echo " ***** If not working for the first time restart your computer ***** "
+echo ""
 sudo pkg install -y linux-c7
 sudo pkg install -y nvidia-driver nvidia-xconfig
 sudo sysrc kld_list+="nvidia_modeset"
