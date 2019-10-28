c79910685c2b7fef2cef6657d8b5c2377d42ebf7
crn1 <gluvajic@aol.com>
2019-10-28 17:47:09 +0100

    Fixed NVIDIA bug. Again

--- NVIDIA.sh
+++ NVIDIA.sh
@@ -6,9 +6,9 @@ sudo kldload nvidia-modeset
 sudo mkdir -p /usr/local/etc/X11/xorg.conf
 
 sudo bash -c 'echo "Section \"Device\"" > /usr/local/etc/X11/xorg.conf.d/driver-nvidia.conf'
-sudo bash -c 'echo "\tIdentifier \"NVIDIA Card\"" >> /usr/local/etc/X11/xorg.conf.d/driver-nvidia.conf'
-sudo bash -c 'echo "\tVendorName \"NVIDIA Coorporation\"" >> /usr/local/etc/X11/xorg.conf.d/driver-nvidia.conf'
-sudo bash -c 'echo "\tDriver \"nvidia\"" >> /usr/local/etc/X11/xorg.conf.d/driver-nvidia.conf'
+sudo bash -c 'echo "	Identifier \"NVIDIA Card\"" >> /usr/local/etc/X11/xorg.conf.d/driver-nvidia.conf'
+sudo bash -c 'echo "	VendorName \"NVIDIA Coorporation\"" >> /usr/local/etc/X11/xorg.conf.d/driver-nvidia.conf'
+sudo bash -c 'echo "	Driver \"nvidia\"" >> /usr/local/etc/X11/xorg.conf.d/driver-nvidia.conf'
 sudo bash -c 'echo "EndSection" >> /usr/local/etc/X11/xorg.conf.d/driver-nvidia.conf'
 
 sudo nvidia-xconfig
