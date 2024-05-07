7aa93aafe5b6ea48298ca9ffb95f805e4409e7e6
crn1 <gluvajic@aol.com>
2024-05-07 15:31:40 +0200

    adapted for the lastest arch workflow

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,8 +1,28 @@
 # ARCH -- pacman
-sudo pacman -Sy install fish python ranger pip wget curl vim git gcc cmake
+sudo pacman -Syy install fish python ranger pip wget curl vim git gcc cmake
 
 #ARCH -- pacman -- GUI
-sudo pacman -Sy install xorg xorg-server xorg-apps xorg-xinit i3-gaps i3status i3blocks rxvt-unicode urxvt-perls dmenu arandr transmission-gtk sxiv chromium vlc evince ubuntu-font gedit extra/setxkbmap keepassxc libreoffice nautilus spotify-launcher pamixer openssh #python-pywal feh
+sudo pacman -Sy install xorg xorg-server xorg-apps xorg-xinit i3-gaps i3status i3blocks rxvt-unicode urxvt-perls dmenu arandr transmission-gtk sxiv vlc evince ubuntu-font gedit extra/setxkbmap keepassxc libreoffice nautilus spotify-launcher pamixer openssh go unzip flameshot python-pip docker #python-pywal feh
+
+#Download and install yay
+git clone https://aur.archlinux.org/yay-git.git ~/yay-git
+cd ~/yay-git && makepkg -si
+rm -rf ~/yay-git
+
+#Install Google Chrome
+yay -Sy google-chrome
+
+#Install beeper
+wget -O /usr/local/bin/beeper https://download.beeper.com/linux/appImage/x64
+sudo chmod +x /usr/local/bin/beeper
+
+#Install espanso
+wget -O /usr/local/bin/espanso 'https://github.com/federico-terzi/espanso/releases/download/v2.2.1/Espanso-X11.AppImage'
+sudo chmod +x /usr/local/bin/espanso
+
+#Download Dropbox
+wget -O ~/dropbox-installer "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf ~/dropbox-installer
+rm ~/dropbox-installer
 
 #enable lightdm
 #sudo systemctl enable lightdm
@@ -50,13 +70,11 @@ sudo ln -sf "$PWD/config/redshift.conf" /root/.config/redshift.conf
 #fish syslink
 ln -sf "$PWD/config/fish/config.fish" ~/.config/fish/config.fish
 ln -sf "$PWD/config/fish/fish_variables" ~/.config/fish/fish_variables
-
 sudo ln -sf "$PWD/config/fish/config.fish" ~/.config/fish/config.fish
 sudo ln -sf "$PWD/config/fish/fish_variables" ~/.config/fish/fish_variables
 
 #Delete motd message
 sudo rm -f /etc/motd
-
 
 #Setup vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -72,6 +90,3 @@ echo 'Enter password for fish shell inegration'
 chsh -s `which fish`
 fish -c "set fish_greeting"
 sudo fish -c "set fish_greeting"
-
-#download Dropbox THIS NEEDS TO BE LAST!
-cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
--- profile
+++ profile
@@ -1,7 +1,11 @@
-TERM=urxvt; 	export TERM
-
-EDITOR=vim;   	export EDITOR
-PAGER=less;  	export PAGER
+export TERM=urxvt
+export EDITOR=vim
+export PAGER=less
+export BROWSER=google-chrome-stable
+export MAILCLIENT=google-chrome-stable
+export GIT_EDITOR=vim
+export VISUAL=gedit
+export PDFREADER=evince
 
 # Query terminal size; useful for serial lines.
 # if [ -x /usr/bin/resizewin ] ; then /usr/bin/resizewin -z ; fi
