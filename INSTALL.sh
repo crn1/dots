5501d30e983d27f2e11f594738683c6c22e5d2b3
crn1 <gluvajic@aol.com>
2024-05-07 16:09:26 +0200

    fix

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,9 +1,51 @@
 # ARCH -- pacman
-sudo pacman -Syy install fish python ranger pip wget curl vim git gcc cmake
+sudo pacman -Syy
+sudo pacman -S fish
+sudo pacman -S python
+sudo pacman -S ranger
+sudo pacman -S python-pip
+sudo pacman -S wget
+sudo pacman -S curl
+sudo pacman -S vim
+sudo pacman -S git
+sudo pacman -S gcc
+sudo pacman -S cmake
+
+#ARCH -- pacman -- GUI -- Xorg
+sudo pacman -S xorg
+sudo pacman -S xorg-server
+sudo pacman -S xorg-apps
+sudo pacman -S xorg-xinit
+sudo pacman -S extra/xorg-setxkbmap
 
 #ARCH -- pacman -- GUI
-sudo pacman -Sy install xorg xorg-server xorg-apps xorg-xinit i3-gaps i3status i3blocks rxvt-unicode urxvt-perls dmenu arandr transmission-gtk sxiv vlc evince ubuntu-font gedit extra/setxkbmap keepassxc libreoffice nautilus spotify-launcher pamixer openssh go unzip flameshot python-pip docker
-# + python-pywal feh
+sudo pacman -S i3-gaps
+sudo pacman -S i3status
+sudo pacman -S i3blocks
+sudo pacman -S rxvt-unicode
+sudo pacman -S urxvt-perls
+sudo pacman -S dmenu
+sudo pacman -S arandr
+sudo pacman -S transmission-gtk
+sudo pacman -S sxiv
+sudo pacman -S vlc
+sudo pacman -S evince
+sudo pacman -S gedit
+sudo pacman -S keepassxc
+sudo pacman -S libreoffice
+sudo pacman -S pamixer
+sudo pacman -S openssh
+sudo pacman -S go
+sudo pacman -S unzip
+sudo pacman -S docker
+sudo pacman -S nautilus
+
+#ARCH -- pacman -- additional
+sudo pacman -S flameshot
+sudo pacman -S spotify-launcher
+sudo pacman -S extra/ttf-ubuntu-font-family
+#sudo pacman -S feh
+#sudo pacman -S python-pywal
 
 #Download and install yay
 git clone https://aur.archlinux.org/yay-git.git ~/yay-git
